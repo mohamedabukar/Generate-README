32fcfde3d09fd5770abe5dd200e02da0dad2a956
Mohamed <mohamed.h.abukar@gmail.com>
2021-07-06 18:29:24 -0500

    template readme added.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Generate-README
+# ng undefined
+# Description
+gns,
+# Table of contents
+* [Installation](#installation) 
+* [usage](#usage)
+* [contribution Guidelines](#contribution-guidelines)
+* [test instructions](#test-instrcutions)
+* [license](#license)
+* [Github](#github)
+* [Email](#email)
+# Installation
+gngng
+# Usage
+gnglgklg
+# Contribution guidelines
+ggnng
+# Test instructions
+gngng
+# license
+gngn is the license that this this application is covered under.
+# Questions
+Here is my gitHub.[Guthub](https://github.com/gngng)
+Here is my email.gngn
+    
--- index.js
+++ index.js
@@ -21,33 +21,32 @@ const questions = [
     {
         type: 'input',
         message: 'Enter usage information.',
-        name: 'usageInformation',
+        name: 'usage',
     },
     {
         type: 'input',
         message: 'Enter contribution guidelines.',
-        name: 'contributionGuidelines',
+        name: 'contribution',
     },
     {
         type: 'input',
         message: 'Enter test instructions.',
-        name: 'testInstructions',
+        name: 'test',
     },
    {
-       type: 'list',
+       type: 'input',
        message: 'License for application.',
-       name: 'License',
-        choices: ["IBM","ISC", "MIT"]
+       name: 'license',
    },
    {
        type: 'input',
        message: 'What is your GitHub username',
-       name: 'Github'
+       name: 'github'
    },
    {
        type: 'input',
        message: 'What is your email?',
-       name: 'Email'
+       name: 'email'
    }
 ];
 
@@ -55,26 +54,42 @@ inquirer
 .prompt(questions)
 .then(response => {
     console.log(response)
-    writeToFile(response);
+    writeToFile('README.md', response);
 })
 
 // TODO: Create a function to write README file
-function writeToFile(README, data) {
-    var writeFile = `
-#${response.title}
-#Description
+function writeToFile(README, response) {
+    var writeFile = 
+`# ${response.title} ${response.License}
+# Description
 ${response.description}
-#Table of contents
-[Installation](#installation-instructions) 
-[usage](#usage)
-[contribution Guidelines](#contribution-guidelines)
-[test instructions](#test-instrcutions)
-[license](#license)
-[Github](#github)
-[Email](#email)
+# Table of contents
+* [Installation](#installation) 
+* [usage](#usage)
+* [contribution Guidelines](#contribution-guidelines)
+* [test instructions](#test-instrcutions)
+* [license](#license)
+* [Github](#github)
+* [Email](#email)
+# Installation
+${response.installationInstructions}
+# Usage
+${response.usage}
+# Contribution guidelines
+${response.contribution}
+# Test instructions
+${response.test}
+# license
+${response.license} is the license that this this application is covered under.
+# Questions
+* My gitHub.[Github](https://github.com/${response.github})
+* Here is my email.${response.email}
     `
-    fs.writeFile("./README.md",writeFile);
-}
+    fs.writeFile(README,writeFile, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });;
+    }
 
 // TODO: Create a function to initialize app
 function init() {}
